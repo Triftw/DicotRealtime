0b55508b3d999c15652538485171b87771e32281
Triftw <yeuluen.tw@gmail.com>
2020-03-20 16:51:07 +0800

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 13:37:59 2020
+
+@author: Trif
+"""
+
+import tkinter as tk
+window = tk.Tk()
+window.title('BMI App')
+window.geometry('800x600')
+window.configure(background='white')
+window.mainloop()
