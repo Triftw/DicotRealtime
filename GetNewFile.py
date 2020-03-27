466036a79c10ddaa16bab5f8310674335b14a224
Triftw <yeuluen.tw@gmail.com>
2020-03-27 20:49:55 +0800

    realtime test done

--- GetNewFile.py
+++ GetNewFile.py
@@ -7,7 +7,14 @@ Created on Sun Mar  8 17:38:46 2020
 
 import glob
 import os
+from time import sleep as sp
 
 list_of_files = glob.glob('C:\\Users\\Trif\\Desktop\\NewFile\*') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
-print (latest_file)
+while True:
+    sp(1)
+    list_of_files = glob.glob('C:\\Users\\Trif\\Desktop\\NewFile\*') # * means all if need specific format then *.csv
+    if max(list_of_files, key=os.path.getctime) != latest_file:
+        print(max(list_of_files, key=os.path.getctime))
+        latest_file = max(list_of_files, key=os.path.getctime)
+    
